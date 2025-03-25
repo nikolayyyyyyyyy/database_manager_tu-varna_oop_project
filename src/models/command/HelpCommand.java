e81b58b74b0da01d7380e706e4214f79a6746173
Nikolay Nikolaev <cholyo999999@gmail.com>
2025-03-25 16:49:41 +0200

    commit!

--- src/models/command/HelpCommand.java
+++ src/models/command/HelpCommand.java
@@ -2,6 +2,7 @@ package models.command;
 import interfaces.Command;
 import models.common.MessageLogger;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 
@@ -13,13 +14,33 @@ public class HelpCommand implements Command {
     private final Map<String,String> help;
 
     public HelpCommand(){
+        this.help = new LinkedHashMap<>();
+        initSupportedCommands(this.help);
+    }
 
-        this.help = Map.of("open <file>", "opens the given file",
-                "close <file>", "closes the given file",
-                "save <file>", "saves the given file",
-                "saveas <file>", "saves file in new file",
-                "help","prints this information",
-                "exit","exists the program");    }
+    /**
+     * Инициализирам командите който поддържа системата
+     * @param help картата с възможните команди
+     */
+    private void initSupportedCommands(Map<String, String> help) {
+        help.put("open <file>","opens the given file");
+        help.put("close <file>","closes the given file");
+        help.put("save <file>","saves the given file");
+        help.put("saveas <file>","saves file in new file");
+        help.put("help","prints this information");
+        help.put("exit","exists the program");
+        help.put("showtables","Print name of loaded tables");
+        help.put("describe <name>","Show column name and type");
+        help.put("print <name>","Print in pages the rows");
+        help.put("select <column-n> <value> <table name>","Show all row from table that match at given index the value");
+        help.put("update <table name> <search column n> <search value> <target column n> <target value>", "Update table where in the search column contains the search value in the target index column with the target value");
+        help.put("delete <table name> <search column n> <search value>","Delete all row in table where column contains the value");
+        help.put("insert <table name> <column n...>","Ïnsert new row in the table");
+        help.put("innerjoin <table 1> <column n1> <table 2> <column 2>","Joins the tables where the columns match");
+        help.put("rename <old name> <new name>","Rename the table if the name is not taken");
+        help.put("count <table name> <search column n> <search value>","Print the count of rows that match in given column the value");
+        help.put("aggregate <table name> <search column n> <search value> <target column n> <operation>","Do aggregation into the column of table with target column where in column n contains the searched value");
+    }
 
     /**
      * Изпълнява командата за извеждане на помощна информация за наличните команди.
